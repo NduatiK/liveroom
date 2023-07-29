fdc4a8a8c7640e7b4bda0351d4dffc31cfa0ee22
Basile Nouvellet <b.nouvellet@gmail.com>
2023-07-30 01:24:55 +0200

    Track mouse move on screensharing video

--- chrome-extension/src/components/Overlay.svelte
+++ chrome-extension/src/components/Overlay.svelte
@@ -3,54 +3,69 @@
 
   let started = false;
 
+  let mouseMoveCompatibleStyle = document.createElement("style");
+  mouseMoveCompatibleStyle.textContent = `
+    video {
+      /* NOTE: Make sure the video element is hoverable & clickable (it is set to pointer-events: none on Google Meet) */
+      pointer-events: auto !important;
+    }
+    [jsaction^="mousedown:"] {
+      /* NOTE: Deactivate the menu displayed when hovering, else it clashes with our mouse move listener on the video element */
+      pointer-events: none !important;
+    }
+  `;
+
   let selectVideoElStyle = document.createElement("style");
   selectVideoElStyle.textContent = `
-          #overlay, #overlay * {
-            visibility: visible !important;
-          }
-          video {
-            visibility: visible !important;
-            /* NOTE: Make sure the video element is hoverable & clickable (it is set to pointer-events: none on Google Meet) */
-            pointer-events: auto !important;
-            cursor: pointer;
-          }
-          /* NOTE: Unfortunately, we can't add a pseudo-element to the video element itself, so we add it to its parent */
-          *:has(> video) {
-            position: relative;
-          }
-          *:has(> video)::before {
-            visibility: visible !important;
-            z-index: 9999;
-            opacity: 0;
-            content: "";
-            position: absolute;
-            inset: 0;
-            outline: 2px solid white;
-            background-color: white;
-            transition: opacity 0.1s ease-out;
-          }
-          *:has(> video:hover)::before {
-            opacity: 0.5;
-          }
-          /* NOTE: Deactivate the menu displayed when hovering, else it clashes with our video element hover */
-          [jsaction] {
-          /* [jsaction*="mousedown"] { */
-            pointer-events: none !important;
-          }
-          body *:not(video) {
-            visibility: hidden !important;
-          }
-          body {
-            /* NOTE: Default dark background on Google Meet */
-            background-color: #202124 !important;
-          }
-        `;
+    #overlay, #overlay * {
+      visibility: visible !important;
+    }
+    video {
+      visibility: visible !important;
+      /* NOTE: Make sure the video element is hoverable & clickable (it is set to pointer-events: none on Google Meet) */
+      pointer-events: auto !important;
+      cursor: pointer;
+    }
+    /* NOTE: Unfortunately, we can't add a pseudo-element to the video element itself, so we add it to its parent */
+    *:has(> video) {
+      position: relative;
+    }
+    *:has(> video)::before {
+      visibility: visible !important;
+      z-index: 9999;
+      opacity: 0;
+      content: "";
+      position: absolute;
+      inset: 0;
+      outline: 2px solid white;
+      background-color: white;
+      transition: opacity 0.1s ease-out;
+    }
+    *:has(> video:hover)::before {
+      opacity: 0.5;
+    }
+    /* NOTE: Deactivate the menu displayed when hovering, else it clashes with our video element hover */
+    [jsaction] {
+    /* [jsaction*="mousedown"] { */
+      pointer-events: none !important;
+    }
+    body *:not(video) {
+      visibility: hidden !important;
+    }
+    body {
+      /* NOTE: Default dark background on Google Meet */
+      background-color: #202124 !important;
+    }
+  `;
 
-  let resizeObserver;
+  let resizeObserver: ResizeObserver;
 
-  let screensharingVideoEl;
-  let screensharingVideoElWidth;
-  let screensharingVideoElHeight;
+  let screensharingVideoEl: HTMLVideoElement;
+  let screensharingVideoElWidth: number;
+  let screensharingVideoElHeight: number;
+
+  let mouseX: number;
+  let mouseY: number;
 
   // Observe the screensharing video element dimensions
   $: if (screensharingVideoEl) {
@@ -66,9 +81,35 @@
     resizeObserver.disconnect();
   }
 
+  // Observe the mouse coordinates
+  $: if (screensharingVideoEl) {
+    document.head.appendChild(mouseMoveCompatibleStyle);
+    screensharingVideoEl.addEventListener("mousemove", handleMouseMove);
+  } else {
+    if (mouseMoveCompatibleStyle.parentNode === document.head) {
+      document.head.removeChild(mouseMoveCompatibleStyle);
+    }
+  }
+
+  // LIFECYCLE
+
   onDestroy(() => {
     resizeObserver?.disconnect();
+    screensharingVideoEl?.removeEventListener("mousemove", handleMouseMove);
+    if (selectVideoElStyle.parentNode === document.head) {
+      document.head.removeChild(selectVideoElStyle);
+    }
+    if (mouseMoveCompatibleStyle.parentNode === document.head) {
+      document.head.removeChild(mouseMoveCompatibleStyle);
+    }
   });
+
+  // HELPERS
+
+  function handleMouseMove(e) {
+    mouseX = e.layerX;
+    mouseY = e.layerY;
+  }
 </script>
 
 <div id="overlay">
@@ -104,10 +145,14 @@
     <p>Click on the screensharing video</p>
   {/if}
 
-  {#if screensharingVideoEl && screensharingVideoElWidth && screensharingVideoElHeight}
+  {#if screensharingVideoEl}
     <p>
       Screensharing dimensions:
       <b>{screensharingVideoElWidth}x{screensharingVideoElHeight}</b>
+    </p>
+    <p>
+      Mouse coordinates:
+      <b>{mouseX}, {mouseY}</b>
     </p>
   {/if}
 </div>
