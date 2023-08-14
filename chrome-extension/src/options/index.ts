1cbabbf38da5dad0122e52bc1601597577a740a8
Basile Nouvellet <b.nouvellet@gmail.com>
2023-08-14 15:55:28 +0100

    Run npm run format --prefix chrome-extension

--- chrome-extension/src/options/index.ts
+++ chrome-extension/src/options/index.ts
@@ -4,9 +4,9 @@ import { storage } from "src/storage";
 const target = document.getElementById("app");
 
 function render() {
-    storage.get().then(({ count }) => {
-        new Options({ target, props: { count } });
-    });
+  storage.get().then(({ count }) => {
+    new Options({ target, props: { count } });
+  });
 }
 
 document.addEventListener("DOMContentLoaded", render);
