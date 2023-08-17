ad954d0ff2ec5930aa3aa323e81dc913ec8f9e7a
Basile Nouvellet <b.nouvellet@gmail.com>
2023-08-17 15:19:19 +0100

    Refactor

---
+++ chrome-extension/src/components/UserName.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import type { User } from "../types/User";
+
+  // mandatory
+  export let user_name: User["name"];
+  // optional
+  export let hide: boolean = false;
+  export let style: string = "";
+</script>
+
+<span class="user-name" data-hide={hide} {style}> {user_name}</span>
+
+<style>
+  .user-name {
+    padding: 4px 10px;
+    font-size: 14px;
+    line-height: 20px;
+    font-weight: 600;
+    color: black;
+    background-color: var(--color);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    border-radius: 9999px;
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05); /* Tailwind 'shadow-sm' */
+  }
+  .user-name[data-hide="true"] {
+    display: none;
+  }
+</style>
---
+++ chrome-extension/src/components/UsersCursors.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+  import type { User } from "../types/User";
+  import UserName from "./UserName.svelte";
+
+  export let me_id: string;
+  export let users: { [key: User["id"]]: User };
+  export let screensharingVideoEl: HTMLVideoElement;
+  export let screensharingVideoElWidth: number;
+  export let screensharingVideoElHeight: number;
+
+  function placeCursorsContainerNextToScreensharingVideoEl(
+    cursorsContainerEl: HTMLElement
+  ) {
+    screensharingVideoEl.parentElement.style.position = "relative";
+    screensharingVideoEl.parentElement.appendChild(cursorsContainerEl);
+    return {
+      destroy() {
+        if (screensharingVideoEl?.parentElement) {
+          delete screensharingVideoEl.parentElement.style.position;
+          if (screensharingVideoEl.parentElement.contains(cursorsContainerEl)) {
+            screensharingVideoEl.parentElement.removeChild(cursorsContainerEl);
+          }
+        }
+      },
+    };
+  }
+</script>
+
+<div id="users-container" use:placeCursorsContainerNextToScreensharingVideoEl>
+  {#each Object.values(users) as user (user.id)}
+    <div
+      id="user-{user.id}"
+      class="user"
+      data-isself={user.id == me_id}
+      style="
+          --color: {user.color};
+          --x: {(parseFloat(user.x) / 100) * screensharingVideoElWidth}px;
+          --y: {(parseFloat(user.y) / 100) * screensharingVideoElHeight}px;
+        "
+    >
+      <svg
+        width="23"
+        viewBox="0 0 27 24"
+        fill="currentColor"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        class="cursor"
+      >
+        <path
+          d="M2.2706 0.0593359L25.4277 8.05957H25.45926C25.65896 8.13153 25.83096 8.25952 25.95221 8.42653C26.07357 8.59354 26.13851 8.79166 26.13851 8.99459C26.13851 9.19751 26.07357 9.39564 25.95221 9.56265C25.83096 9.72966 25.65896 9.85765 25.45926 9.92962L15.3543 13.7698L11.3124 23.37C11.2344 23.5561 11.0994 23.7156 10.9248 23.828C10.7503 23.9402 10.5443 24.0002 10.3335 24C10.1172 24 9.9061 23.9365 9.7291 23.8184C9.552 23.7004 9.4176 23.5332 9.344 23.34L0.9233 1.33937C0.8555 1.16076 0.8426 0.967504 0.8861 0.782189C0.9297 0.596873 1.0278 0.427163 1.1691 0.292901C1.3105 0.158639 1.4891 0.0653762 1.6841 0.0240151C1.8792 -0.0173461 2.0826 -0.00509504 2.2706 0.0593359Z"
+        />
+      </svg>
+      <UserName
+        user_name={user.name}
+        hide={user.id == me_id}
+        style="position: absolute; top: 20px; left: 16px"
+      />
+      <!-- <div
+          class="halo"
+          data-show={user.is_mouse_down || user.is_shift_key_down}
+          /> -->
+    </div>
+  {/each}
+</div>
+
+<style>
+  #users-container {
+    position: absolute;
+    inset: 0;
+    overflow: hidden;
+    pointer-events: none;
+  }
+
+  .user {
+    z-index: 10;
+    position: absolute;
+    top: 0;
+    left: 0;
+    user-select: none;
+    transform: translate(var(--x), var(--y));
+  }
+
+  .user .cursor {
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: var(--color);
+    transform-origin: top left;
+    transform: rotate(6deg);
+  }
+  .user[data-isself="true"] .cursor {
+    display: none;
+  }
+
+  /* .user .halo {
+    transform: scale(0);
+    z-index: -1;
+    position: absolute;
+    top: -60px;
+    left: -60px;
+    width: 120px;
+    height: 120px;
+    border-radius: 9999px;
+    background-color: var(--color);
+    opacity: 0.25;
+    transition: transform 0.15s ease-out;
+    /* Tailwind 'shadow-sm'
+    box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);
+  }
+  .user .halo[data-show="true"] {
+    transform: scale(1);
+  } */
+</style>
