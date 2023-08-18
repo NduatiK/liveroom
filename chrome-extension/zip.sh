1515360dce65275bf45f51aec7aaefdf1252c19e
Basile Nouvellet <b.nouvellet@gmail.com>
2023-08-18 18:14:56 +0100

    Fix CI

--- chrome-extension/zip.sh
+++ chrome-extension/zip.sh
@@ -2,4 +2,4 @@
 version=$(grep -Eo '"version"\s*:\s*"[^"]*"' manifest.json | grep -Eo '[0-9]+(\.[0-9]+)*')
 
 # Zip dist folder with version from manifest.json
-zip -r "dist/liveroom_chrome_extension_$version.zip" dist/*
+zip -r "dist/liveroom-chrome-extension-$version.zip" dist/*
