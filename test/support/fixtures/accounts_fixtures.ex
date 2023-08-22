8c46c8ad4f9e031329d2bbd10ebe4f0b15875551
Basile Nouvellet <b.nouvellet@gmail.com>
2023-08-22 11:05:27 +0100

    Fix tests

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -10,8 +10,7 @@ defmodule Liveroom.AccountsFixtures do
   def valid_user_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
       email: unique_user_email(),
-      password: valid_user_password(),
-      picture_url: "https://img.com/picture.png"
+      password: valid_user_password()
     })
   end
 
