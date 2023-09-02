9e47cf8be0c5934ec58d62d892d0c2d452f71ef9
Basile Nouvellet <b.nouvellet@gmail.com>
2023-09-02 14:56:48 +0100

    Update :req dependency

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Liveroom.MixProject do
       {:live_state, "~> 0.6.1"},
       {:cors_plug, "~> 3.0.3"},
       {:libcluster, "~> 3.3.2"},
-      {:req, "~> 0.3.6"},
+      {:req, "~> 0.4.1"},
       {:plug_canonical_host, "~> 2.0"},
       {:phoenix, "~> 1.7.2"},
       {:phoenix_ecto, "~> 4.4"},
